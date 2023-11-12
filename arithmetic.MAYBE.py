66ac96ea86bb5fcb147f2db87579f2f52e543562
Pythonatron <pythonatron@gmail.com>
2023-11-11 22:34:56 -0600

    Reworkings

--- arithmetic.MAYBE.py
+++ arithmetic.MAYBE.py
@@ -1,30 +1,30 @@
 def arithmetic(x):
-    
-    alph = {
-        "a": "1", "b": "2", "c": "3", "d": "4", "e": "5", "f": "6", "g": "7",
-        "h": "8", "i": "9", "j": "10", "k": "11", "l": "12", "m": "13", "n": "14",
-        "o": "15", "p": "16", "q": "17", "r": "18", "s": "19", "t": "20", "u": "21",
-        "v": "22", "w": "23", "x": "24", "y": "25", "z": "26"
+    alphabet = {
+        "a":"1", "b":"2", "c":"3", "d":"4", "e":"5", "f":"6", "g":"7",
+        "h":"8", "i":"9", "j":"10", "k":"11", "l":"12", "m":"13", "n":"14",
+        "o":"15", "p":"16", "q":"17", "r":"18", "s":"19", "t":"20", "u":"21",
+        "v":"22", "w":"23", "x":"24", "y":"25", "z":"26"
         }
     
-    print("Choose an additive number.")
-    print("try to keep it relatively small.")
-    print("... but it doesn't have to.")
-    addval = int(input("Number: "))
+    print("Choose an additive number")
+    print("Try to keep it small")
+    print("... but it doesn't have to")
+    addval = int(input("Your Number: "))
     result = []
+    parts = str(x.lower()).split()
     
-    x = x.lower()
-    parts = str(x).split()
     for part in parts:
         if part.isnumeric():
             part = int(part)
             result.append(part + addval)
         else:
-            outage = []
+            encoded = []
             for char in part:
                 if char.isalpha():
-                    outage.append(int(alph[char]) + addval)
-            outage = " ".join(map(str, outage))
-            result.append(outage)
+                    encoded.append(int(alphabet[char]) + addval)
+            encoded = " ".join(map(str, encoded))
+            result.append(encoded)
     result = " || ".join(map(str, result))
     return result
+
+#print(arithmetic("This, is a TeSt!. 123 1 2 3"))
--- geometric.NOTDONE.py
+++ geometric.NOTDONE.py
@@ -1,37 +1,30 @@
 def geometric(x):
-    result = []
-    alf = {
-        "a": "1", "b": "2", "c": "3", "d": "4", "e": "5", "f": "6", "g": "7",
-        "h": "8", "i": "9", "j": "10", "k": "11", "l": "12", "m": "13", "n": "14",
-        "o": "15", "p": "16", "q": "17", "r": "18", "s": "19", "t": "20", "u": "21",
-        "v": "22", "w": "23", "x": "24", "y": "25", "z": "26"
+    alphabet = {
+        "a":"1", "b":"2", "c":"3", "d":"4", "e":"5", "f":"6", "g":"7",
+        "h":"8", "i":"9", "j":"10", "k":"11", "l":"12", "m":"13", "n":"14",
+        "o":"15", "p":"16", "q":"17", "r":"18", "s":"19", "t":"20", "u":"21",
+        "v":"22", "w":"23", "x":"24", "y":"25", "z":"26"
         }
 
-    def text_to_numeric(s):
-        numeric_form = ""
-        for char in s:
-            if char.isalpha():
-                numeric_form += alf[char]
-            elif char.isnumeric():
-                numeric_form += char
-            else:
-                numeric_form += char 
-        return numeric_form
-    print("Choose a multiplier, try to keep it relatively small")
-    print("..but i'm not your mother *shrug*")
-    mult = int(input("Multiplier: "))
-    parts = str(x).split()
+    print("Choose a multiplier")
+    print("Try to keep it small")
+    print("... but it doesn't have to")
+    mult = int(input("Your Number: "))
+    result = []
+    parts = str(x.lower()).split()
+
     for part in parts:
         if part.isnumeric():
             part = int(part)
             result.append(part * mult)
         else:
-            numeric_form = text_to_numeric(part)
-            tri_parts = []
+            encoded = []
             for char in part:
                 if char.isalpha():
-                    tri_parts.append(int(alf[char]) * mult)
-            tri_parts = " ".join(map(str, tri_parts))
-            result.append(tri_parts)
-    result = " ".join(map(str, result))
+                    encoded.append(int(alphabet[char]) * mult)
+            encoded = " ".join(map(str, encoded))
+            result.append(encoded)
+    result = " || ".join(map(str, result))
     return result
+
+#print(geometric("This, is a TeSt!. 123 1 2 3"))
